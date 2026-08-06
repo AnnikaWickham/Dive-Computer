f1ccad92ddc0d64e05f6a1843fe2b5620f6143f2
Annika Wickham <arwickha@ucsc.edu>
2026-08-05 20:58:39 -0700

    todo update

--- diveLogic.js
+++ diveLogic.js
@@ -4,6 +4,7 @@
 
 #       - A nitrogen tank that fills as you input the planned bottom time
         - Add bubbles that rise from the scuba diver every 2ish seconds
+        - Little Question mark icon to give info about color depth, NDL and safety stop
 
 #       - SUPER FUTURE implement the actual physics and algs that PADI used to calc the tables
 #       - NDL can be affected by water temp, age, weight, gas mixture, exertion, and altitude.
