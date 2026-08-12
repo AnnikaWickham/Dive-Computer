0dac1f94f3b82afef561857dc71b0cce789144ef
Annika Wickham <arwickha@ucsc.edu>
2026-08-12 14:20:32 -0700

    todo update

--- diveLogic.js
+++ diveLogic.js
@@ -3,7 +3,6 @@
         - Little Question mark icon to give info about color depth, NDL and safety stop
         - Safety Stop logic for dive 2.
         - Pressure Groups for dive 2.
-        - Eddies issue
         
 #       - SUPER FUTURE implement the actual physics and algs that PADI used to calc the tables
 #       - NDL can be affected by water temp, age, weight, gas mixture, exertion, and altitude.
